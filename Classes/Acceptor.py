e0df3199de47c77416a31b6b13ad7849e501f3d3
nickmarton <nickmarton1993@gmail.com>
2015-11-10 19:41:02 -0500

    pickle Acceptor object function 'save'

--- Classes/Acceptor.py
+++ Classes/Acceptor.py
@@ -17,15 +17,42 @@ class Acceptor(object):
 
     def __init__(self):
         """Construct Acceptor object."""
-
         self._maxPrepare = -1
         self._accNum = None
         self._accVal = None
         self._is_Acceptor = True
 
+    def save(self, filename="./Acceptor.pkl"):
+        """
+        Write Acceptor object to memory, i.e., store maxPrepare, accNum, and
+        accVal.
+
+        Done easily with pickle module.
+        """
+
+        if type(filename) != str:
+            print type(filename)
+            raise TypeError("filename must be a string")
+
+        if filename[-4:] != ".pkl":
+            raise ValueError("filename must have .pkl extension")
+
+        import pickle
+        with open(filename, 'wb') as f:
+            pickle.dump(self, f)
+
+    def __str__(self):
+        """Implement str(Acceptor)."""
+        return str((self._maxPrepare, self._accNum, self._accVal))
+
+    def __repr__(self):
+        """Implement repr(Acceptor)."""
+        return self.__str__()
+
 def main():
     """Quick tests."""
-    pass
+    a = Acceptor()
+    print a
 
 if __name__ == "__main__":
     main()
